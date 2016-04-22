3470ad971deea502b577319c96e965739da9fd40
ceberous <cerbus.collin@gmail.com>
2016-04-22 19:57:37 -0400

    Added Gorrilavid.in Support , suprisingly , video.js plays flv files with the mime type tag of mp4 just fine

--- app_api/controllers/theWatchFlixMain.js
+++ app_api/controllers/theWatchFlixMain.js
@@ -4,14 +4,10 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
-// sendJSONResponse( res , 200 , body );
-// sendJSONResponse( res , 404 , null );
 var sendJSONResponse = function( res , status , content ) {
     if (status) {res.status(status);}
     res.json(content);
 };
-
-
 
 module.exports.searchTVShow = function( req , res ) {
 	
@@ -33,107 +29,82 @@ module.exports.searchTVShow = function( req , res ) {
 
 };
 
-var parseForProviders = function( res , body ) {
+// ==================================module.exports.searchTVShowEpisoddeForProviders======================================================================
+	var parseForProviders = function( res , body ) {
 
-	var results = [];
+		var results = [];
 
-	var $ = cheerio.load(body);
+		var vodlocker = [];
+		var gorillavid = [];
 
-	var episodeName = $('span.list-top').text();
-	episodeName = episodeName.split(" - ")[1];
-	//console.log("Episode Name = " + episodeName);
-	results.push(episodeName);
+		var $ = cheerio.load(body);
 
-	var $linkSearch = $("#linktable a");
-	$linkSearch.add($linkSearch.find('*'));
-	$linkSearch.each( function( i , e ) {
-		//console.log( $(e).attr("title") + " = " + $(e).attr("href") );
-		var p = $(e).attr("title");
-		p = ( p === undefined ) ? "null" : p.toString().trim();
-		if ( 
-			p === "vodlocker.com" /*|| 
-			p === "gorillavid.in" ||
-			p === "allmyvideos.net" ||
-			p === "thevideo.me" */
-		) {
-			var obj = {
-				provider: p,
-				url: $(e).attr("href")
-			};
-			results.push(obj);
-		}
+		var episodeName = $('span.list-top').text();
+		episodeName = episodeName.split(" - ")[1];
+		//console.log("Episode Name = " + episodeName);
+		results.push(episodeName);
 
-	});
-
-	sendJSONResponse( res , 200 , results );
-
-};
-
-module.exports.searchTVShowEpisodeForProviders = function( req , res ) {
-
-	var searchURL = "http://thewatchseries.to/episode/" + req.params.showID + "_s" + req.params.season + "_e" + req.params.episode + ".html";
-	console.log("Grabbing: " + searchURL );
-
-	request( searchURL , function( error , response , body ) {
-
-		if ( !error && response.statusCode === 200 ) {
-			if (body) {
-				parseForProviders( res , body );
+		var $linkSearch = $("#linktable a");
+		$linkSearch.add($linkSearch.find('*'));
+		$linkSearch.each( function( i , e ) {
+			//console.log( $(e).attr("title") + " = " + $(e).attr("href") );
+			var p = $(e).attr("title");
+			p = ( p === undefined ) ? "null" : p.toString().trim();
+			
+			if ( p === "vodlocker.com" ) {
+				var obj = {
+					provider: p,
+					url: $(e).attr("href")
+				};				
+				vodlocker.push(obj);
 			}
+			else if ( p === "gorillavid.in" ) {
+				var obj = {
+					provider: p,
+					url: $(e).attr("href")
+				};				
+				gorillavid.push(obj);
+			}
+
+		});
+
+		if ( gorillavid[0] === undefined ) {
+
+			results.push("blank.mp4");
+			if ( vodlocker[0] === undefined ) {
+				results.push("blank.mp4");
+			}
+			else {
+				results.push( vodlocker[0] );
+				results.push( vodlocker[1] );
+			}
+
+		}
+		else if ( vodlocker[0] === undefined ) {
+
+			results.push("blank.mp4");
+
 		}
 		else {
-			if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
-		}		
+			console.log("	debug(found 1 of each)");
+			results.push(gorillavid[0]);
+			results.push(vodlocker[0]);
+		}
 
-	});
+		sendJSONResponse( res , 200 , results );
 
-};
+	};
 
+	module.exports.searchTVShowEpisodeForProviders = function( req , res ) {
 
-var parseForVodlockerMP4 = function( res , body ) { // needs undefined error checking like real bad
+		var searchURL = "http://thewatchseries.to/episode/" + req.params.showID + "_s" + req.params.season + "_e" + req.params.episode + ".html";
+		console.log("Grabbing: " + searchURL );
 
-	
-	var start , start2 , end;
-	start = body.indexOf( "player_code" );
-	start2 = body.indexOf( "file:" , start );
-	end = body.indexOf( "v.mp4"  , start );
-	var mp4URL = body.substring( start2 + 7 , end + 5 );
-	
-	/*
-	var $ = cheerio.load(body);
-	var $linkSearch = $("#player_code");
-	$linkSearch.add($linkSearch.find('*'));
-	$linkSearch.each( function( i , e ) {
-		//console.log( $(e).attr("title") + " = " + $(e).attr("href") );
-		console.log( $(e) );
-	});
-	*/
-
-	sendJSONResponse( res , 200 , mp4URL );
-
-};
-
-
-var parseForVodlockerHash = function( res , body , id ) {
-
-	var searchURL = "http://vodlocker.com/" + id;
-
-	var start , end;
-	start = body.indexOf( "name=\"hash\"" , 0 );
-	end = body.indexOf( ">" , start );
-	var hash = body.substring( start , end );
-	hash = hash.split(" ")[1];
-	hash = hash.split("\"")[1];
-
-	setTimeout(function(){
-		console.log("waiting on vodlocker countdown ....");
-
-		// console.log( "		FOUND HASH: " + hash );
-		request.post( { url: searchURL , form: { op: "download1" , id: id , hash: hash , imhuman: "Proceed to video" } } , function( error , response , rBody ) {
+		request( searchURL , function( error , response , body ) {
 
 			if ( !error && response.statusCode === 200 ) {
-				if (rBody) {
-					parseForVodlockerMP4( res , rBody );
+				if (body) {
+					parseForProviders( res , body );
 				}
 			}
 			else {
@@ -142,178 +113,284 @@ var parseForVodlockerHash = function( res , body , id ) {
 
 		});
 
-	}, 1500 );
+	};
+// ==================================module.exports.searchTVShowEpisoddeForProviders======================================================================
 
-};
 
-
-var searchVodlocker = function( res , id ) {
-
-	var searchURL = "http://vodlocker.com/" + id;
-	console.log( "Searching: " + searchURL );
-
-	// need the stupid hash to post to vodlocker
-	// x-www-form-urlencoded
-	// op: download1,
-	// id: id,
-	// hash: ??????
-
-	request( searchURL , function( error , response , body ) {
-
-		if ( !error && response.statusCode === 200 ) {
-			if (body) {
-				parseForVodlockerHash( res , body , id );
-			}
-		}
-		else {
-			if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
-		}			
-
-	});
-
+// ==================================module.exports.searchProvider========================================================================================
 	
+	//==================================Vodlocker.com====================================================
+		var parseForVodlockerMP4 = function( res , body ) { // needs undefined error checking like real bad
 
-};
-
-module.exports.searchProvider = function( req , res ) {
-
-	var provider = req.params.provider;
-	var id = req.params.url;
-
-	switch ( provider ) {
-
-		case "vodlocker":
-			searchVodlocker( res , id );
-			break;
-		case "gorillavid":
-			searchGorillavid( res , id );
-			break;
-		case "allmyvideos":
-			searchAllmyvideos( res , id );
-			break;
-		case "thevideo":
-			searchTheVideo( res , id );
-			break;
-		default:
-			console.log("unknown provider");
-			sendJSONResponse( res , 404 , null );
-			break;
-
-	}
-	
-
-};
-
-
-module.exports.search = function( req , res ) {
-
-	// req.params.searchString
-
-	// http://thewatchseries.to/show/search-shows-json
-	// POST | x-www-form-urlencoded
-	// term: req.params.searchString
-
-	var searchURL = "http://thewatchseries.to/show/search-shows-json";
-	//console.log("Searching: " + req.params.searchString);
-	request.post( { url: searchURL , form: { term: req.params.searchString } } , function( error , response , body ) {
-
-		if ( !error && response.statusCode === 200 ) {
-			if (body) {
-				sendJSONResponse( res , 200 ,  body );
-			}
-		}
-		else {
-			if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
-		}		
-
-	});	
-
-
-};
-
-var parseLatestResults = function( res , latestResults  ) {
-
-	var actualResults = [];
-
-	for ( var i = 0; i < latestResults.length; ++i ) {
-
-		var $ = cheerio.load(latestResults[i]);
-
-		var $linkSearch = $(".listings a").has("span");
-		$linkSearch.add($linkSearch.find('*'));
-		$linkSearch.each( function( i , e ) {
 			
-			var cache = $(e);
-			var date = cache.children()[0].children[0]["data"];
-			if ( date.length > 9 ) {
-				var link = cache.attr("href");
-				link = link.split("/episode/")[1];
-				link = link.split(".html")[0];
-				//console.log(link);
-				link = link.split("_");
-				var episode = link.pop();
-				episode = episode.substring( 1 , episode.length ); // remove "e"
-				var season = link.pop();
-				season = season.substring( 1 , season.length ); // remove "s"
-				var showID = link.join("_");
-				var obj = {
-					showID: showID,
-					seasonNumber: season,
-					episodeNumber: episode,
-					date: date
-				};	
-				//console.log( obj.showID + " | S: " + obj.seasonNumber + " | E: " + obj.episodeNumber + " | Date: " obj.date );
-				actualResults.push( obj );
+			var start , start2 , end;
+			start = body.indexOf( "player_code" );
+			start2 = body.indexOf( "file:" , start );
+			end = body.indexOf( "v.mp4"  , start );
+			var mp4URL = body.substring( start2 + 7 , end + 5 );
+			
+			/*
+			var $ = cheerio.load(body);
+			var $linkSearch = $("#player_code");
+			$linkSearch.add($linkSearch.find('*'));
+			$linkSearch.each( function( i , e ) {
+				//console.log( $(e).attr("title") + " = " + $(e).attr("href") );
+				console.log( $(e) );
+			});
+			*/
 
-			}
+			sendJSONResponse( res , 200 , mp4URL );
 
-		});
-
-	}
-
-	sendJSONResponse( res , 200 , actualResults );
-
-};
+		};
 
 
-var loadLatestURL = "http://thewatchseries.to/latest/";
-var latestIndex = 1;
-var latestResults = [];
-var loadLatestHelper = function( res ) {
+		var parseForVodlockerHash = function( res , body , id ) {
 
-	if ( latestIndex < 6 ) {
+			var searchURL = "http://vodlocker.com/" + id;
+
+			var start , end;
+			start = body.indexOf( "name=\"hash\"" , 0 );
+			end = body.indexOf( ">" , start );
+			var hash = body.substring( start , end );
+			hash = hash.split(" ")[1];
+			hash = hash.split("\"")[1];
+
+			setTimeout(function(){
+				console.log("waiting on vodlocker countdown ....");
+
+				// console.log( "		FOUND HASH: " + hash );
+				request.post( { url: searchURL , form: { op: "download1" , id: id , hash: hash , imhuman: "Proceed to video" } } , function( error , response , rBody ) {
+
+					if ( !error && response.statusCode === 200 ) {
+						if (rBody) {
+							parseForVodlockerMP4( res , rBody );
+						}
+					}
+					else {
+						if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
+					}		
+
+				});
+
+			}, 1500 );
+
+		};
+
+
+		var searchVodlocker = function( res , id ) {
+
+			var searchURL = "http://vodlocker.com/" + id;
+			console.log( "Searching: " + searchURL );
+
+			// need the stupid hash to post to vodlocker
+			// x-www-form-urlencoded
+			// op: download1,
+			// id: id,
+			// hash: ??????
+
+			request( searchURL , function( error , response , body ) {
+
+				if ( !error && response.statusCode === 200 ) {
+					if (body) {
+						parseForVodlockerHash( res , body , id );
+					}
+				}
+				else {
+					if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
+				}			
+
+			});
+
+			
+
+		};
+	//==================================Vodlocker.com====================================================
+
+	//==================================Gorrillavid.in====================================================
+
+		var parseGorrilaVid = function( res , body ) {
+
+			//console.log(body);
+
+			var start , end , result;
+			start = body.indexOf( "file:" );
+			end = body.indexOf( "video.flv" , start );
+			result = body.substring( start + 7 , end + 9 );
+
+			sendJSONResponse( res , 200 , result );
+
+		};
+
+		var searchGorillavid = function( res , id ) {
+
+			var searchURL = "http://gorillavid.in/" + id;
+			console.log( "Searching: " + searchURL );
+
+			request.post( { url: searchURL , form: { op: "download1" , id: id , method_free: "Free Download" } } , function( error , response , body ) {
+
+				if ( !error && response.statusCode === 200 ) {
+					if (body) {
+						parseGorrilaVid( res , body );
+					}
+				}
+				else {
+					if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
+				}		
+
+			});	
+
+		};
+
+	//==================================Gorrillavid.in====================================================
+
+	module.exports.searchProvider = function( req , res ) {
+
+		var provider = req.params.provider;
+		var id = req.params.url;
+
+		switch ( provider ) {
+
+			case "vodlocker":
+				searchVodlocker( res , id );
+				break;
+			case "gorillavid":
+				searchGorillavid( res , id );
+				break;
+			case "allmyvideos":
+				searchAllmyvideos( res , id );
+				break;
+			case "thevideo":
+				searchTheVideo( res , id );
+				break;
+			default:
+				console.log("unknown provider");
+				sendJSONResponse( res , 404 , null );
+				break;
+
+		}
 		
-		var x = loadLatestURL + latestIndex;
-		console.log("Grabbing: " + x);
-		request( x , function( error , response , body ) {
+
+	};
+// ==================================module.exports.searchProvider========================================================================================
+
+
+// theWatchTVSeries.to/search
+// ==================================module.exports.search===============================================
+	module.exports.search = function( req , res ) {
+
+		// req.params.searchString
+
+		// http://thewatchseries.to/show/search-shows-json
+		// POST | x-www-form-urlencoded
+		// term: req.params.searchString
+
+		var searchURL = "http://thewatchseries.to/show/search-shows-json";
+		//console.log("Searching: " + req.params.searchString);
+		request.post( { url: searchURL , form: { term: req.params.searchString } } , function( error , response , body ) {
 
 			if ( !error && response.statusCode === 200 ) {
 				if (body) {
-					latestIndex += 1;
-					latestResults.push(body);
-					loadLatestHelper( res );
+					sendJSONResponse( res , 200 ,  body );
 				}
 			}
 			else {
 				if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
-			}			
+			}		
 
-		});		
-		
-	}
-	else {
-
-		parseLatestResults( res , latestResults );
-
-	}
+		});	
 
 
-};
+	};
+// ==================================module.exports.search===============================================
 
-module.exports.loadLatestEpisodes = function( req , res ) {
 
-	latestResults = [];
-	latestIndex = 1;
-	loadLatestHelper( res );
+// ==================================module.exports.loadLatestEpisodes===================================
+	var parseLatestResults = function( res , latestResults  ) {
 
-};
+		var actualResults = [];
+
+		for ( var i = 0; i < latestResults.length; ++i ) {
+
+			var $ = cheerio.load(latestResults[i]);
+
+			var $linkSearch = $(".listings a").has("span");
+			$linkSearch.add($linkSearch.find('*'));
+			$linkSearch.each( function( i , e ) {
+				
+				var cache = $(e);
+				var date = cache.children()[0].children[0]["data"];
+				if ( date.length > 9 ) {
+					var link = cache.attr("href");
+					link = link.split("/episode/")[1];
+					link = link.split(".html")[0];
+					//console.log(link);
+					link = link.split("_");
+					var episode = link.pop();
+					episode = episode.substring( 1 , episode.length ); // remove "e"
+					var season = link.pop();
+					season = season.substring( 1 , season.length ); // remove "s"
+					var showID = link.join("_");
+					var obj = {
+						showID: showID,
+						seasonNumber: season,
+						episodeNumber: episode,
+						date: date
+					};	
+					//console.log( obj.showID + " | S: " + obj.seasonNumber + " | E: " + obj.episodeNumber + " | Date: " obj.date );
+					actualResults.push( obj );
+
+				}
+
+			});
+
+		}
+
+		sendJSONResponse( res , 200 , actualResults );
+
+	};
+
+	var loadLatestURL = "http://thewatchseries.to/latest/";
+	var latestIndex = 1;
+	var latestResults = [];
+	var loadLatestHelper = function( res ) {
+
+		if ( latestIndex < 6 ) {
+			
+			var x = loadLatestURL + latestIndex;
+			console.log("Grabbing: " + x);
+			request( x , function( error , response , body ) {
+
+				if ( !error && response.statusCode === 200 ) {
+					if (body) {
+						latestIndex += 1;
+						latestResults.push(body);
+						loadLatestHelper( res );
+					}
+				}
+				else {
+					if ( error ) { console.log( error ); sendJSONResponse( res , 404 , null ); }
+				}			
+
+			});		
+			
+		}
+		else {
+
+			parseLatestResults( res , latestResults );
+
+		}
+
+
+	};
+
+	module.exports.loadLatestEpisodes = function( req , res ) {
+
+		latestResults = [];
+		latestIndex = 1;
+		loadLatestHelper( res );
+
+	};
+// ==================================module.exports.loadLatestEpisodes===================================
+
+
+
